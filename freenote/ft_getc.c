1f2fedf92c00745903c8fcf230f7cf81416b39b5
rnomoto42 <r.nomoto12345@gmail.com>
2025-04-06 15:21:27 +0900

    the function became unnecessary

--- freenote/ft_getc.c
+++ freenote/ft_getc.c
@@ -6,20 +6,19 @@
 
 int ft_getc(int fd)
 {
-    static char buf[BUFSIZ]; //read to this valuable
-    static int fd_store = -1;
+    static char buf; //read to this valuable
+    static int fd_store = -2; //initialized to a number which is not returned by open()
     static ssize_t read_size;
     
     static int i = 0;
-    //printf("fd: %d\n", fd);
 
     if (fd_store == fd)
         i++;
     else
     {
         i = 0;
-        read_size = read(fd, buf, sizeof buf); 
-        //printf("read_size: %zu\n", read_size);
+        printf("kkk");
+        read_size = read(fd, &buf, 1); 
         if (read_size == -1)
             return EOF;
         else if (read_size == 0)
@@ -29,7 +28,7 @@ int ft_getc(int fd)
     if (read_size == 0)
         return EOF;
     read_size --;
-    return buf[i];
+    return buf;
 }
 
 int main(void)
@@ -38,60 +37,14 @@ int main(void)
     int c;
 
     fd = open("hello.txt", O_RDONLY);
-    while (1)
+    c = ft_getc(fd);
+    if (c == EOF)
     {
-        c = ft_getc(fd);
-        if (c == EOF)
-            break;
-        printf("%c", c);
+        printf("error\n");
+        return 1;
     }
+    printf("%c", c);
     close(fd);
-
-    // int fd = open("hello.txt", O_RDONLY);
-    // int c = ft_getc(fd);
-    // if (c == EOF)
-    //     printf("error\n");
-    // else
-    //     printf("%c\n", c);
-    
-    // int fd2 = open("number.txt", O_RDONLY);
-    // c = ft_getc(fd2);
-    // if (c == EOF)
-    //     printf("error\n");
-    // else
-    //     printf("%c\n", c);
-    
-    // c = ft_getc(fd);
-    // if (c == EOF)
-    //     printf("error\n");
-    // else
-    //     printf("%c\n", c);
-
-    // close(fd);
-    // close(fd2);
-
-    // FILE *fp = fopen("hello.txt", "r");
-    // int c = getc(fp);
-    // if (c == EOF)
-    //     printf("error\n");
-    // else
-    //     printf("%c\n", c);
-    
-    // FILE *fp2 = fopen("number.txt", "r");
-    // c = getc(fp2);
-    // if (c == EOF)
-    //     printf("error\n");
-    // else
-    //     printf("%c\n", c);
-    
-    // c = getc(fp);
-    // if (c == EOF)
-    //     printf("error\n");
-    // else
-    //     printf("%c\n", c);
-
-    // fclose(fp);
-    // fclose(fp2);
 
     return 0;
 }
