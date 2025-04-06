c593db144321ae59fe5c2d6c666293307c109c23
rnomoto42 <r.nomoto12345@gmail.com>
2025-04-06 19:04:36 +0900

    normkilled

--- project/get_next_line.c
+++ project/get_next_line.c
@@ -6,43 +6,21 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 19:28:08 by rnomoto           #+#    #+#             */
-/*   Updated: 2025/04/06 17:34:16 by rnomoto          ###   ########.fr       */
+/*   Updated: 2025/04/06 18:15:52 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char *buf;
-    char c;
-    size_t i = 0;
-    size_t buf_size = 10;
-    
-    c = 'a'; //initiate
-    buf = (char *)ft_calloc(sizeof(char), buf_size + 1);
-    if (buf == NULL)
-        return NULL;
-    while (c != '\n')
-    {
-        if (i == buf_size)
-        {
-            buf = double_buffer(buf, buf_size);
-            if (buf == NULL)
-                return NULL;
-            buf_size *= 2;
-        }
-        c = read_c(fd);
-        if (c == -1 && i == 0)
-        {
-            free(buf);
-            return NULL;
-        }
-        else if (c == -1)
-            break;
-        buf[i] = c;
-        i++;
-    }
-    return buf;
+	char	*buf;
+	size_t	buf_size;
+
+	buf_size = 10;
+	buf = (char *)ft_calloc(sizeof(char), buf_size);
+	if (buf == NULL)
+		return (NULL);
+	buf = put_line(buf, buf_size, fd);
+	return (buf);
 }
--- project/get_next_line_utils.c
+++ project/get_next_line_utils.c
@@ -6,13 +6,11 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 18:52:59 by rnomoto           #+#    #+#             */
-/*   Updated: 2025/04/06 17:20:40 by rnomoto          ###   ########.fr       */
+/*   Updated: 2025/04/06 19:01:29 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <stdint.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
@@ -37,32 +35,63 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	return (tmp);
 }
 
-char *double_buffer(char *old_buf, size_t size)
+char	*double_buffer(char *old_buf, size_t size)
 {
-    char *new_buf;
-    size_t i = 0;
-    size_t new_size = size * 2;
-    
-    new_buf = (char *)ft_calloc(sizeof(char), new_size + 1);
-    if (new_buf == NULL)
-        return (NULL);
-    while (i < size)
-    {
-        new_buf[i] = old_buf[i];
-        i++;
-    }
-    free(old_buf);
-    return new_buf;
+	char	*new_buf;
+	size_t	i;
+	size_t	new_size;
+
+	i = 0;
+	new_size = size * 2;
+	new_buf = (char *)ft_calloc(sizeof(char), new_size);
+	if (new_buf == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		new_buf[i] = old_buf[i];
+		i++;
+	}
+	free(old_buf);
+	return (new_buf);
 }
 
-int read_c(int fd)
+int	read_c(int fd)
 {
-    char c;
-    ssize_t read_check;
+	char	c;
+	ssize_t	read_check;
 
-    read_check = read(fd, &c, 1);
-    if (read_check == -1 || read_check == 0)
-        return -1;
+	read_check = read(fd, &c, 1);
+	if (read_check == -1 || read_check == 0)
+		return (-1);
+	return (c);
+}
 
-    return c;
+char	*put_line(char *buf, size_t size, int fd)
+{
+	char	c;
+	size_t	i;
+
+	i = 0;
+	c = 'a';
+	while (c != '\n')
+	{
+		if (i == size - 1)
+		{
+			buf = double_buffer(buf, size);
+			if (buf == NULL)
+				return (NULL);
+			size *= 2;
+		}
+		c = read_c(fd);
+		if (c == -1 && i == 0)
+		{
+			free(buf);
+			return (NULL);
+		}
+		else if (c == -1)
+			break ;
+		buf[i] = c;
+		i++;
+	}
+	return (buf);
 }
