eac1c8afb91d625810f5e789f82b3bc322fea42e
rnomoto42 <r.nomoto12345@gmail.com>
2025-04-12 20:38:06 +0900

    pointer is useful

--- freenote/array_move.c
+++ freenote/array_move.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// int main(void)
+// {
+//     static char array[10] = "abcdefghi\0";
+//     int i = 0;
+
+//     printf("%s\n", array);
+//     while (i < 10 - 2)
+//     {
+//         array[i] = array[i + 2];
+//         i++;
+//     }
+//     printf("%s\n", array);
+
+//     return 0;
+// }
 
 int main(void)
 {
-    static char array[10] = "abcdefghi\0";
-    int i = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+    char *mem = (char *)malloc(sizeof(char) * 10);
 
-    printf("%s\n", array);
-    while (i < 10 - 2)
-    {
-        array[i] = array[i + 2];
-        i++;
-    }
-    printf("%s\n", array);
+    printf("%zd\n", sizeof(mem));
 
     return 0;
 }
